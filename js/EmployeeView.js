6f052bf5fe597c1b72211c2fcb3f0a328615da7a
graemefawcett <graemefawcett@chickenoncow.com>
2014-04-08 20:49:26 -0400

    camer

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -2,6 +2,7 @@ var EmployeeView = function(employee) {
     this.initialize = function() {
         this.el = $('<div/>');
         this.el.on('click', '.add-location-btn', this.addLocation);
+        this.el.on('click', '.change-pic-btn', this.changePicture);
     };
 
     this.initialize();
@@ -19,10 +20,35 @@ var EmployeeView = function(employee) {
                 $('.location', this.el).html(position.coords.latitude + ',' + position.coords.longitude);
             },
             function() {
-                alert('Error getting location');
+                app.showAlert('Error getting location');
             });
         return false;
     };
+
+    this.changePicture = function(event) {
+        event.preventDefault();
+
+        if (!navigator.camera) {
+            app.showAlert("Camera API not supported", "Error");
+            return;
+        }
+
+        var options = {
+            quality: 50,
+            destinationType: Camera.DestinationType.DATA_URL,
+            sourceType: 1,
+            encodingType: 0
+        };
+
+        navigator.camera.getPicture(
+            function(imageData) {
+                $('.employee-image', this.el).attr('src', 'data:image/jpeg;base64,' + imageData);
+            },
+            function() {
+                app.showAlert("Error taking picture", "Error");
+            },
+            options);
+    }
 }
 
 EmployeeView.template = Handlebars.compile($("#employee-tpl").html());
